88fdb758b0d8852bd5be2a20b750d54fa2f0e3b2
oneilld136 <oneilld136@gmail.com>
2019-03-20 15:58:41 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,7 +15,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -30,5 +30,5 @@ def third_challenge
    }
   }
 
-  
+
 end
